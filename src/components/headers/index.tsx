018822c99aa509257327b14a8f1097ca787259cf
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-08-24 19:43:52 +0400

    update

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -4,7 +4,7 @@ export default function Headers() {
     return <>
         <Head>
             <title>Exceed your expectations | Amet</title>
-            <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
         </Head>
     </>
 }
---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+export default function NotFoundPage() {
+    return <>
+        <span>Page was not found</span>
+    </>
+}
