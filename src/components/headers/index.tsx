d245c5b10a0c8b2a1a67a0fa65497b1cbc181c68
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-08-24 19:46:55 +0400

    Update index.tsx

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -4,7 +4,7 @@ export default function Headers() {
     return <>
         <Head>
             <title>Exceed your expectations | Amet</title>
-            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link rel="shortcut icon" href="/favicon.ico?version=3"/>
         </Head>
     </>
 }
