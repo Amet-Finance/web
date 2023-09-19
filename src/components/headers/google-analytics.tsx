9210a25633aac9b6686f3de1d924e0ae692fdfad
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-09-20 00:22:39 +0400

    update

---
+++ src/components/headers/google-analytics.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+    return (
+        <>
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-JG562EL5SM`} />
+
+            <Script strategy="lazyOnload" id=''>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function 
+                    gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-JG562EL5SM', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+        </>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ export default function App({Component, pageProps}: AppProps) {
             </main>
             <ModalHandler/>
             <ToastContainer />
+            <GoogleAnalytics/>
         </Provider>
     </>
 }
