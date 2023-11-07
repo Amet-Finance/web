e966a1ce30366b3ba0e67904d7fc502a7792d8a3
TheUnconstrainedMind <141995559+amet-finance-core@users.noreply.github.com>
2023-11-07 20:14:18 +0400

    up

--- src/pages/swap/index.tsx
+++ src/pages/swap/index.tsx
@@ -62,51 +62,51 @@ export default function SwapPage() {
 
     console.log(`result`, result);
 
-    const outputTokenDecimals = result?.tokens?.[tokenOut].decimals
-    const output = result?.outputAmount / 10 ** outputTokenDecimals
+    // const outputTokenDecimals = result?.tokens?.[tokenOut].decimals
+    // const output = result?.outputAmount / 10 ** outputTokenDecimals
 
     return <>
-        <div className='flex items-center justify-center w-full'>
-            <div className='flex flex-col gap-2 w-40 bg-b1  min-w-500 rounded'>
-                <div className='flex flex-col gap-2 px-4 py-5'>
-                    <div className='flex justify-between items-center gap-10 w-full'>
-                        <span className='text-xl'>You Pay</span>
-                        <SettingsSVG/>
-                    </div>
-                    <div className='flex justify-between items-center'>
-                        <div className='flex flex-col gap-2'>
-                            <input type="number" placeholder='The amount to swap'
-                                   className='bg-transparent rounded w-full text-lg'
-                                   id='fromAmount'
-                                   onChange={onChange}
-                                   autoFocus/>
-                            <span className='text-g2'>~$4,307</span>
-                        </div>
-                        <div className='flex border border-solid border-w1 p-2 rounded bg-b2'>
-                            <span>ETH</span>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col gap-2 bg-b2 px-2 py-5 m-2 rounded'>
-                    <span className='text-xl'>You Receive</span>
-                    <div className='flex justify-between items-center'>
-                        <div className='flex flex-col'>
-                            <span>{output}</span>
-                            <span className='text-g2'>~${result?.receivedUsd?.toFixed(2)}</span>
-                        </div>
-                        <div className='flex border border-solid border-w1 p-2 rounded bg-b2'>
-                            <span>DAI</span>
-                        </div>
-                    </div>
-                    <div className="h-px w-full bg-g5"/>
-                    <span>1ETH = 4031 DAI</span>
-                    <div className='flex flex-col items-center gap-2'>
-                        <button className='w-full bg-white  text-black rounded'>Swap</button>
-                        <span className='text-xs'>Powered by KyberSwap</span>
-                    </div>
-                </div>
+        {/*<div className='flex items-center justify-center w-full'>*/}
+        {/*    <div className='flex flex-col gap-2 w-40 bg-b1  min-w-500 rounded'>*/}
+        {/*        <div className='flex flex-col gap-2 px-4 py-5'>*/}
+        {/*            <div className='flex justify-between items-center gap-10 w-full'>*/}
+        {/*                <span className='text-xl'>You Pay</span>*/}
+        {/*                <SettingsSVG/>*/}
+        {/*            </div>*/}
+        {/*            <div className='flex justify-between items-center'>*/}
+        {/*                <div className='flex flex-col gap-2'>*/}
+        {/*                    <input type="number" placeholder='The amount to swap'*/}
+        {/*                           className='bg-transparent rounded w-full text-lg'*/}
+        {/*                           id='fromAmount'*/}
+        {/*                           onChange={onChange}*/}
+        {/*                           autoFocus/>*/}
+        {/*                    <span className='text-g2'>~$4,307</span>*/}
+        {/*                </div>*/}
+        {/*                <div className='flex border border-solid border-w1 p-2 rounded bg-b2'>*/}
+        {/*                    <span>ETH</span>*/}
+        {/*                </div>*/}
+        {/*            </div>*/}
+        {/*        </div>*/}
+        {/*        <div className='flex flex-col gap-2 bg-b2 px-2 py-5 m-2 rounded'>*/}
+        {/*            <span className='text-xl'>You Receive</span>*/}
+        {/*            <div className='flex justify-between items-center'>*/}
+        {/*                <div className='flex flex-col'>*/}
+        {/*                    <span>{output}</span>*/}
+        {/*                    <span className='text-g2'>~${result?.receivedUsd?.toFixed(2)}</span>*/}
+        {/*                </div>*/}
+        {/*                <div className='flex border border-solid border-w1 p-2 rounded bg-b2'>*/}
+        {/*                    <span>DAI</span>*/}
+        {/*                </div>*/}
+        {/*            </div>*/}
+        {/*            <div className="h-px w-full bg-g5"/>*/}
+        {/*            <span>1ETH = 4031 DAI</span>*/}
+        {/*            <div className='flex flex-col items-center gap-2'>*/}
+        {/*                <button className='w-full bg-white  text-black rounded'>Swap</button>*/}
+        {/*                <span className='text-xs'>Powered by KyberSwap</span>*/}
+        {/*            </div>*/}
+        {/*        </div>*/}
 
-            </div>
-        </div>
+        {/*    </div>*/}
+        {/*</div>*/}
     </>
 }
