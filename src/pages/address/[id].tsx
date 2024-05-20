58e11494d1a58af0745dc0efd6f479489990ce37
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-05-20 09:02:39 +0400

    update

--- src/pages/address/[id].tsx
+++ src/pages/address/[id].tsx
@@ -6,18 +6,29 @@ import {PageId} from "@/components/pages/constants";
 import AccountStore from "@/store/redux/account";
 import CloudAPI from "@/modules/api/cloud";
 
-export default function AddressPage({accountExtendedFormat}: Readonly<{ accountExtendedFormat: AccountExtendedFormat }>) {
+export default function AddressPage({accountExtendedFormat}: Readonly<{
+    accountExtendedFormat: AccountExtendedFormat
+}>) {
     return <AddressId accountExtendedFormat={accountExtendedFormat}/>
 }
 
 export async function getServerSideProps({query}: any) {
-    const holdings = await GraphqlAPI.getAccountInformation({address: query.id, chainId: defaultChain.id})
-    const account = await CloudAPI.getUser(query.id);
+    const address = query.id;
+    const holdings = await GraphqlAPI.getAccountInformation({address, chainId: defaultChain.id})
+    const account = await CloudAPI.getUser(address);
 
     const accountExtendedFormat = {
         ...account,
         ...holdings
     }
 
-    return {props: {accountExtendedFormat, pageId: PageId.AddressId}}
+    return {
+        props: {
+            accountExtendedFormat,
+            pageId: PageId.AddressId,
+            meta: {
+                title: `Account Explorer - Manage Your Profile & Investments | Address: ${address.toLowerCase()} | Amet Finance`
+            }
+        }
+    }
 }
